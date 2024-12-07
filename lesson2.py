c9ab89e2179345e059a1d189fff515b5db045ab8
Gilbert Omosa <omosagilbert@gmail.com>
2024-12-07 04:17:20 +0300

    added lesson2.py types of variables, how to name them and use them in print statements

---
+++ lesson2.py
@@ -0,0 +1,37 @@
+# fundamentals
+# storing data -- variables, databases
+# manipulate the data --
+# display the data --
+from datetime import date
+
+# variables
+
+# integer
+age = 27
+print(age)
+
+# float
+weight = 187.9
+print(weight)
+
+# string
+name = 'Omosa'
+print(name)
+
+# boolean -- true or false --
+is_mature = True
+print(is_mature)
+
+
+# camelCasing
+isMatureEnough = True
+distanceToTheSun = 500
+
+# no spacing when naming -- only use underscore or camelCase
+date_today = '07-12-2024'
+
+# cannot start variable name with a number -- but can use number in the name
+person_1 = 'Gilbert'
+
+
+print(person_1, 'travelled', distanceToTheSun, 'million KM', 'on this date', date_today)
