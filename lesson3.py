f4b8032451689403f9ad3596886b322be297dced
Gilbert Omosa <omosagilbert@gmail.com>
2024-12-07 04:30:35 +0300

    added lesson3.py lesson on operators

---
+++ lesson3.py
@@ -0,0 +1,16 @@
+
+# operators
+x = 100
+y = 20
+result = x * y - 5 / 2 + 6 * (3 + 4)
+
+print(result)
+print(3/2)
+print(result*3)
+print("----------------------------")
+print(10/3)
+print(10//3)
+print("----------------------------")
+
+sentence = "The quick brown fox jumps over the lazy dog"
+print(sentence.upper())
